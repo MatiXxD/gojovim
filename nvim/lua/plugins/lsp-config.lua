f487c94f35f90506a631fb0d1d06be8721a03296
MatiXxD <dxxmatixxd@gmail.com>
2024-07-09 14:06:42 +0300

    Update lsp-config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,60 +1,73 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"clangd",
-					"golangci_lint_ls",
-					"tsserver",
-					"jdtls",
-					"pyright",
-					"cssls",
-					"html",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-        capabilities = capabilities
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "emmet_ls",
+          "basedpyright",
+          "clangd",
+          "cssls",
+          "html",
+          "jdtls",
+          "lua_ls",
+          "pyright",
+          "quick_lint_js",
+          "tsserver",
+          "golangci_lint_ls",
+        },
       })
-			lspconfig.clangd.setup({
-        capabilities = capabilities
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
       })
-			lspconfig.golangci_lint_ls.setup({
-        capabilities = capabilities
+      lspconfig.basedpyright.setup({
+        capabilities = capabilities,
       })
-			lspconfig.tsserver.setup({
-        capabilities = capabilities
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
       })
-			lspconfig.jdtls.setup({
-        capabilities = capabilities
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
       })
-			lspconfig.pyright.setup({
-        capabilities = capabilities
+      lspconfig.html.setup({
+        capabilities = capabilities,
       })
-			lspconfig.cssls.setup({
-        capabilities = capabilities
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
       })
-			lspconfig.html.setup({
-        capabilities = capabilities
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.quick_lint_js.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.golangci_lint_ls.setup({
+        capabilities = capabilities,
       })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }

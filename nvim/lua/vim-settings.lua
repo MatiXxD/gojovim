5a93e47df7ba7507bd5b87700d83d17bfd2d97b8
MatiXxD <dxxmatixxd@gmail.com>
2024-07-25 13:20:13 +0300

    Add toggle for col and row cursor

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -4,3 +4,15 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
+
+function ToggleCurline()
+	if vim.o.cursorline and vim.o.cursorcolumn then
+		vim.o.cursorline = false
+		vim.o.cursorcolumn = false
+	else
+		vim.o.cursorline = true
+		vim.o.cursorcolumn = true
+	end
+end
+
+vim.api.nvim_set_keymap("n", "<leader>cr", ":lua ToggleCurline()<CR>", { noremap = true, silent = true })

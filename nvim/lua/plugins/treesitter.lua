6641ca30ff0d4130a8140eb0d41473803a9c343a
MatiXxD <ilya_dinesenko@mail.ru>
2024-08-25 01:03:26 +0300

    Fix treesitter highlight with html files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,12 @@ return {
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
-			-- highlight = { enable = true },
-			indent = { enable = true },
-		})
+			highlight = {
+				enable = true,
+				disable = { "html" },
+				additional_vim_regex_highlighting = false,
+			},
+      indent = { enable = true },
+    })
 	end,
 }

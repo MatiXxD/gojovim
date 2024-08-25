3fab6621da24f61ef7a8527ede079c1f6270c01a
MatiXxD <ilya_dinesenko@mail.ru>
2024-08-25 04:23:39 +0300

    Add floding with nvim ufo

---
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,27 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = {
+		"kevinhwang91/promise-async",
+	},
+	config = function()
+		vim.o.foldcolumn = "1"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds, {})
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds, {})
+		vim.keymap.set("n", "zK", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				vim.lsp.buf.hover()
+			end
+		end, {})
+
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
+	end,
+}

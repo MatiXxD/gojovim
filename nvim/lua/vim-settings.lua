7e60ede0fb3b2383b3d413569c764216d88bba4f
MatiXxD <ilya_dinesenko@mail.ru>
2024-08-31 03:08:35 +0300

    Fix folding and relative numbers

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -3,7 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.wo.relativenumber = true
+vim.opt.relativenumber = true
+vim.opt.number = true
 
 function ToggleCurline()
 	if vim.o.cursorline and vim.o.cursorcolumn then

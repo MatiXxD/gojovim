94c53ed21910f5833df76094acac879157355090
MatiXxD <ilya_dinesenko@mail.ru>
2024-09-02 23:17:57 +0300

    Add prettier formatters

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,38 +1,51 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- LUA
-				null_ls.builtins.formatting.stylua,
-				-- RUBY
-				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.diagnostics.rubocop,
-				-- PYTHON
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.djlint.with({
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        -- LUA
+        null_ls.builtins.formatting.stylua,
+        -- RUBY
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
+        -- PYTHON
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.djlint.with({
           args = { "-", "--reformat", "--indent", 2 },
         }),
-				null_ls.builtins.diagnostics.djlint,
-				-- HTML | CSS
-				null_ls.builtins.formatting.prettier.with({
-					filetypes = { "html", "json", "yaml", "markdown", "vue" },
-				}),
-				-- C++
-				null_ls.builtins.formatting.clang_format,
-				-- require("none-ls.diagnostics.cpplint"),
-				-- GO
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.diagnostics.golangci_lint,
-			},
-		})
+        null_ls.builtins.diagnostics.djlint,
+        -- HTML | CSS
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = {
+            "css",
+            "graphql",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "json",
+            "less",
+            "markdown",
+            "scss",
+            "typescript",
+            "typescriptreact",
+            "yaml",
+          },
+        }),
+        -- C++
+        null_ls.builtins.formatting.clang_format,
+        -- require("none-ls.diagnostics.cpplint"),
+        -- GO
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.diagnostics.golangci_lint,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }

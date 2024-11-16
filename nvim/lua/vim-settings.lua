aada02ff6b81e46afb9fc5d669875e3b6074cbe3
MatiXxD <ilya_dinesenko@mail.ru>
2024-11-17 00:22:05 +0300

    Add expandtab

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -1,7 +1,7 @@
+vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
--- vim.cmd("set expandtab")
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
 vim.opt.number = true

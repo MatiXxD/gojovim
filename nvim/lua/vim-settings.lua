537d3e530a8d0bf3c287710a5faf8af608467184
MatiXxD <ilya_dinesenko@mail.ru>
2025-03-28 22:30:54 +0300

    update packages

--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -7,13 +7,13 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 function ToggleCurline()
-  if vim.o.cursorline and vim.o.cursorcolumn then
-    vim.o.cursorline = false
-    vim.o.cursorcolumn = false
-  else
-    vim.o.cursorline = true
-    vim.o.cursorcolumn = true
-  end
+	if vim.o.cursorline and vim.o.cursorcolumn then
+		vim.o.cursorline = false
+		vim.o.cursorcolumn = false
+	else
+		vim.o.cursorline = true
+		vim.o.cursorcolumn = true
+	end
 end
 
 vim.api.nvim_set_keymap("n", "<leader>cr", ":lua ToggleCurline()<CR>", { noremap = true, silent = true })
